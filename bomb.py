0bea07ac1a4ecb6f6baa17cd033ad14e1948682f
Omar Mohamed Essa <omaressa716@gmail.com>
2025-10-08 07:56:57 +0300

    Update bomb.py

--- bomb.py
+++ bomb.py
@@ -1,18 +1,25 @@
 import pygame
+import random
+import os
 from fruit import Fruit
 
 class Bomb(Fruit):
-    def __init__(self, image_path, explosion_images, x, y, speed_x, speed_y):
+    def __init__(self, x, y, speed_x, speed_y):
         """
-        image_path: path to the bomb image
-        explosion_images: list of paths to explosion animation frames
         """
-        super().__init__(image_path, x, y, speed_x, speed_y)
-        self.explosion_frames = [pygame.transform.scale(pygame.image.load(img), (80, 80)) for img in explosion_images]
+        super().__init__("bomb.png", x, y, speed_x, speed_y) # Assuming 'bomb.png' for bomb image
+        self.explosion_frames = []
+        for i in range(1, 6): # Assuming explosion_frame_1.png to explosion_frame_5.png
+            self.explosion_frames.append(pygame.transform.scale(pygame.image.load(os.path.join("assets", f"explosion_frame_{i}.png")), (80, 80)))
         self.exploding = False
         self.explosion_index = 0
         self.explosion_timer = 0
         self.explosion_frame_duration = 5  # frames per explosion image
+
+    def update(self):
+        super().update() # Call parent's update for movement
+        if self.exploding:
+            self.update_explosion()
 
     def explode(self, sound):
         if not self.exploding:
@@ -45,3 +52,11 @@ class Bomb(Fruit):
         else:
             if not self.sliced:
                 screen.blit(self.image, self.rect)
+
+    @staticmethod
+    def spawn_random(screen_width, screen_height):
+        x = random.randint(100, screen_width - 100)
+        y = screen_height + 50
+        speed_x = random.uniform(-4, 4)
+        speed_y = random.uniform(-10, -6) # Slightly different speed for bombs
+        return Bomb(x, y, speed_x, speed_y)
